adf3099e24d542f54c249c462a20a5b6806a8f27
SArtemA <148715539+SArtemA@users.noreply.github.com>
2023-10-24 08:31:10 +0500

    Update EX.py

--- EX.py
+++ EX.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
 cycle = "да"
 while cycle == "да" or cycle == "Да":
-text = input('Введите текст для перевода на английский: ')
-results = translator(text)
-print("Переведенный текст: " + results[0]['translation_text'])
-cycle = input('Хотите продолжить? \nВведите "Да" для продолжения, "Нет" для остановки\n'
+  text = input('Введите текст для перевода на английский: ')
+  results = translator(text)
+  print("Переведенный текст: " + results[0]['translation_text'])
+  cycle = input('Хотите продолжить? \nВведите "Да" для продолжения, "Нет" для остановки\n'
