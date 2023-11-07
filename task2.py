85eddb17b3e5213fa1f522e9211bd2ae5a0ae364
SArtemA <148715539+SArtemA@users.noreply.github.com>
2023-11-08 00:14:38 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -5,6 +5,6 @@ translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en"
 st.title('Team12UrFUProject')
 st.subheader('This app translates russian to english')
 txt = st.text_area("Place russian text there:")
-tr_txt = translator(txt)
+tr_txt = translator(txt)[0]['translation_text']
 st.write(f'Translated text:\n {tr_txt}')
 
