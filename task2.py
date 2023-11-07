0b768fc3b570e5882ecd56c55e54f2b5b3e5a1f1
SArtemA <148715539+SArtemA@users.noreply.github.com>
2023-11-08 00:15:02 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
-st.title('Team12UrFUProject')
+st.title('Team12UrFUProject Task 2')
 st.subheader('This app translates russian to english')
 txt = st.text_area("Place russian text there:")
 tr_txt = translator(txt)[0]['translation_text']
