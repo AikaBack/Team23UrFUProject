4d0caf014196829176e2dbc271e4b702b0132a9c
Lunatik3 <147321002+Lunatik3@users.noreply.github.com>
2023-12-12 22:57:44 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -12,16 +12,16 @@ def test_hello():
     translation_request = TranslationRequest(text='Один')
     response = client.post("/translate", json=translation_request.dict())
     assert response.status_code == 200
-    assert response.json()["translation"] == "One"
+    assert response.json()["translation"].replace(".", "") == "One"
 
 def test_cat():
     translation_request = TranslationRequest(text='Десять')
     response = client.post("/translate", json=translation_request.dict())
     assert response.status_code == 200
-    assert response.json()["translation"] == "Ten"
+    assert response.json()["translation"].replace(".", "") == "Ten"
 
 def test_dog():
     translation_request = TranslationRequest(text='Собака')
     response = client.post("/translate", json=translation_request.dict())
     assert response.status_code == 200
-    assert response.json()["translation"] == "Dog"
+    assert response.json()["translation"].replace(".", "") == "Dog"
