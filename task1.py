baa940cdfe2070ccec26db76ca0930b3bbcc5f66
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-03-31 17:22:08 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,7 @@
 # pip install transformers sentencepiece sacremoses
 from transformers import pipeline
 
+
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
 
 cycle = "да"
