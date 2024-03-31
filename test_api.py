17dfa114582b32aaa41c5b5d370768e35c7f9240
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-03-31 17:12:42 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -2,8 +2,10 @@ from fastapi.testclient import TestClient
 from main import app
 from pydantic import BaseModel
 
+
 class TranslationRequest(BaseModel):
     text: str
+
 
 client = TestClient(app)
 
@@ -14,11 +16,13 @@ def test_hello():
     assert response.status_code == 200
     assert response.json()["translation"].replace(".", "") == "One"
 
+
 def test_cat():
     translation_request = TranslationRequest(text='Десять')
     response = client.post("/translate", json=translation_request.dict())
     assert response.status_code == 200
     assert response.json()["translation"].replace(".", "") == "Ten"
+
 
 def test_dog():
     translation_request = TranslationRequest(text='Собака')
