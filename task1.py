565f171f33f2e54f10d2e4b61011a68c2ffa6b5c
MaksimSkorobogach <skorobogach007@mail.ru>
2024-05-29 18:15:53 +0500

    Revert "Update task1.py"
    This reverts commit baa940cdfe2070ccec26db76ca0930b3bbcc5f66.

--- task1.py
+++ task1.py
@@ -1,7 +1,6 @@
 # pip install transformers sentencepiece sacremoses
 from transformers import pipeline
 
-
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
 
 cycle = "да"
